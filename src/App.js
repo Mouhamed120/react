5d7755fdef09a21ecb3b24c41f450e6d8b2f4852
Mouhamed120 <65665285+Mouhamed120@users.noreply.github.com>
2022-04-16 19:44:42 +0200

    Book List dans cette partie on apprend à bien utiliser les props en reactJs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import "./styles.css";
-
-export default function App() {
+const firstBook = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg",
+  author: "Marilyn Sadler",
+  title: "It's Not Easy Being a Bunny (Beginner Books(R))"
+};
+const secondBook = {
+  image:
+    "https://images-na.ssl-images-amazon.com/images/I/91xUsdujK3L._AC_UL604_SR604,400_.jpg",
+  author: "Dav Pilkey",
+  title: "Cat Kid Comic Club: On Purpose: A Graphic..."
+};
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="bookList">
+      <Book
+        image={firstBook.image}
+        title={firstBook.title}
+        auth={firstBook.author}
+      />
+      <Book
+        image={secondBook.image}
+        title={secondBook.title}
+        auth={secondBook.author}
+      />
     </div>
   );
-}
+};
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <img src={props.image} alt="" />
+      <h5 className="title">{props.title}</h5>
+      <p>{props.auth}</p>
+    </div>
+  );
+};
+
+export default App;
